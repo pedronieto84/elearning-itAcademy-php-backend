ede5cef318062c2a548d55b3de70f2a5fa279e04
Eva Maria <evamariacamacho@gmail.com>
2021-10-04 11:50:14 +0200

    commint create table videos

---
+++ database/migrations/2021_10_04_094833_create_videos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('videoUrl');
+            $table->string('subTitle');
+            $table->unsignedBigInteger('cards_id')->nullable();
+            $table->foreign('cards_id')
+                    ->references('id')
+                    ->on('cards')
+                    ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
