55470e0d11811e1960800e815b1b6628c060dd2c
Eva Maria <evamariacamacho@gmail.com>
2021-10-04 11:51:30 +0200

    commint create table texts

---
+++ database/migrations/2021_10_04_095045_create_texts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTextsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('texts', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->unsignedBigInteger('cards_id')->nullable();
+            $table->foreign('cards_id')
+                    ->references('id')
+                    ->on('cards')
+                    ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('texts');
+    }
+}
