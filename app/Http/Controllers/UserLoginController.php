14f058c1bbdf38d3c6d187620d658cc6a8d4b5fe
Joserobleses <jrobleses@gmail.com>
2021-11-15 15:41:12 +0100

    creacion de login por API modificados api.php y UserLoginController.php

--- app/Http/Controllers/UserLoginController.php
+++ app/Http/Controllers/UserLoginController.php
@@ -6,41 +6,43 @@ use Illuminate\Http\Request;
 
 use App\Models\User;
 
+use Illuminate\Support\Facades\Hash;
+
 class UserLoginController extends Controller
 {
     public function login(Request $request){
 
         
         
-        $fields = $request->validate([
-            'email'=>'required|string|email',
-            'password' =>'required|confirmed'
-        ]);
+       
         
         //check email
+        $datoS[]='';
        
-        $user = User::where('email',$fields['email'])->first();
-       // $user = User::all();
-        dd($user);
-        //check password
-        //if(!$user || !Hash::check($fields['password'],$user->password)){
+       $datos = json_decode($request->getContent(),true); //dd($datos);
+        foreach($datos as $dato){
+            $datoS[] = $dato; 
+        }
+       
+        $user = User::where('email',$datoS[1])->first();
+       
 
         //This will return true or false based on whether or not the password matches.
-            if(!$user || !Hash::check($fields['password'],$user->password)){
+            if(!$user || !Hash::check('password',$datoS[2])){
             return response(['status'=>false,'message'=>'invalid email or password'],401);
         }
 
         //create token
-       // $token = $user->createToken('myapptoken')->plainTextToken;
+       
 
         $response = [
             'status'=>true,
             'message'=>'Login successful!',
             'data' =>[
                 'user'=>$user,
-                //'token'=>$token
+               
             ]
         ];
         return response($response,201);
-    }
+}
 }
